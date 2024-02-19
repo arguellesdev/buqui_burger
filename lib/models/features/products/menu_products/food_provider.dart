a62b5609dfb83fd0f4f113b9cd9d931f03d9e923
Anya Arguelles <shabelitta@Anyas-MacBook-Pro.local>
2024-02-19 19:52:26 -0300

    changes in Food Provider on debug print to check tap response

--- lib/models/features/products/menu_products/food_provider.dart
+++ lib/models/features/products/menu_products/food_provider.dart
@@ -1,11 +1,19 @@
 import 'dart:convert';
 import 'package:buqui_burgers/models/features/products/menu_products/food.dart';
-
+import 'package:flutter/material.dart';
 import '../../../mocks/food_mocks.dart';
 
 class FoodProvider {
-  Food getFood() {
-    return Food.fromJson(jsonDecode(foodJson) as Map<String, dynamic>);
-
+  Type getFood() {
+    Map<String, dynamic> decodedJson =
+        jsonDecode(foodJson) as Map<String, dynamic>;
+    Food food = Food(
+      foodName: decodedJson['food_name'],
+      foodInfo: decodedJson['foodInfo'],
+      burgerType: decodedJson['burgerType'],
+      foodImage: decodedJson['foodImage'],
+    );
+    debugPrint('food debug output = ${food.foodName}, ${food.foodInfo}, ${food.burgerType}');
+    return Food;
   }
 }
