50152ba08b42a40917fa4b173cda6445e02f81b5
Anya Arguelles <shabelitta@192.168.68.120>
2024-02-25 16:12:56 -0700

    changes in menu screen to add swipe

--- lib/src/features/menu/menu.dart
+++ lib/src/features/menu/menu.dart
@@ -1,3 +1,5 @@
+import 'package:buqui_burgers/src/common_widgets/container_theme.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../products/menu_products/food.dart';
 import '../products/menu_products/food_provider.dart';
@@ -28,10 +30,10 @@ class _MyMenuState extends State<MyMenu> {
         title: const Text('The Buqui Bichi Menu'),
         titleSpacing: 1.2,
         titleTextStyle: const TextStyle(
-            fontSize:18 ,
-        fontWeight: FontWeight.bold,
-        overflow: TextOverflow.fade,
-        color: Colors.brown),
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            overflow: TextOverflow.fade,
+            color: Colors.brown),
       ),
       // endDrawer: ,
       body: PageView.builder(
@@ -57,21 +59,50 @@ class _MyMenuState extends State<MyMenu> {
                         Text(
                           foods[index].foodName,
                           style: const TextStyle(
-                              fontSize: 27.0,
-                              fontWeight: FontWeight.bold),
+                              fontSize: 27.0, fontWeight: FontWeight.bold),
                         ),
                         const SizedBox(height: 12),
                         Text(
                           foods[index].burgerType,
                           style: const TextStyle(
-                              fontSize: 19.0),
+                            fontSize: 19.0,
+                            color: Colors.black38,
+                          ),
                         ),
                         const SizedBox(height: 12),
                         Text(
+                          textAlign: TextAlign.center,
                           foods[index].foodInfo,
                           style: const TextStyle(
-                              fontSize: 17.0),
+                            fontSize: 17.0,
+                            color: Colors.black38,
+                            fontFamily: 'Raleway',
+                            letterSpacing: 2,
+                            wordSpacing: 5,
+                          ),
                         ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 200,
+                    height: 46,
+                    decoration: const BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.all(Radius.circular(55)),
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.arrow_left, color: Colors.cyan),
+                        Text(
+                          'Swipe',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.brown,
+                          ),
+                        ),
+                        Icon(Icons.arrow_right, color: Colors.cyan),
                       ],
                     ),
                   )
