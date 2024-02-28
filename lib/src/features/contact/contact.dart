8e9534c2390f4fa7ad5a9fdb4b8f7013c60f93ca
Anya <arguellesdev@gmail.com>
2024-02-28 09:01:27 -0700

    changes in contact

--- lib/src/features/contact/contact.dart
+++ lib/src/features/contact/contact.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import '../../common_widgets/container_theme.dart';
 import '../../routes/b_routes.dart';
+
 
 class ContactBuqui extends StatelessWidget {
   const ContactBuqui({super.key});
@@ -25,8 +27,11 @@ class _BottomNavigationBarExampleState
     extends State<BottomNavigationBarExample> {
   int _selectedIndex = 0;
 
-  static const TextStyle optionStyle =
-      TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.bold, fontFamily: 'Futura');
+  static const TextStyle optionStyle = TextStyle(
+      color: Colors.grey,
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'Futura');
 
   static const List<Widget> _widgetOptions = <Widget>[
     Text(
@@ -50,75 +55,70 @@ class _BottomNavigationBarExampleState
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: const ContainerTheme(),
+        backgroundColor: const Color(0xFFF7F7FB),
+        foregroundColor: Colors.white,
+        title: const Text('Contact us'),
+        titleSpacing: 1.2,
+        titleTextStyle: const TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            overflow: TextOverflow.fade,
+            color: Colors.white),
+        leading: InkWell(
+          onTap:() {GoRouter.of(context).go('/');},
+              child: const Icon(Icons.arrow_back_ios),
+        )
+      ),
       backgroundColor: const Color(0xFFF7F7FB),
       body: Padding(
         padding: const EdgeInsets.only(top: 20, right: 21, left: 21),
-        child: Stack(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Scaffold(
-              appBar: AppBar(
-                flexibleSpace: const ContainerTheme(),
-                backgroundColor: const Color(0xFFF7F7FB),
-                foregroundColor: Colors.white,
-                title: const Text('Contact us'),
-                titleSpacing: 1.2,
-                titleTextStyle: const TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    overflow: TextOverflow.fade,
-                    color: Colors.white),
-              ),
-              body: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      color: Colors.transparent,
-                      height: 200.0,
-                      alignment: Alignment.center,
-                      child: _widgetOptions.elementAt(_selectedIndex),
-                    ),
-                    Image.network(
-                      'https://static.wixstatic.com/media/3efee9_d3d16f0e618040af97b0ad01abcd03b3~mv2.png/v1/fill/w_1184,h_360,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/cervezas_pngs.png',
-                      // 'https://lumari.mx/wp-content/uploads/2019/07/BBN_11-800x533.jpg',
-                      fit: BoxFit.cover,
-                    ),
-                  ],
-                ),
-              ),
-              floatingActionButton: FloatingActionButton(
-                shape: const CircleBorder(),
-                onPressed: _goToHomeScreen,
-                elevation: 1,
-                backgroundColor: const Color(0xFFF9881F),
-                child: const Icon(Icons.home_rounded),
-              ),
-              bottomNavigationBar: BottomNavigationBar(
-                backgroundColor: const Color(0xFFF9881F),
-                items: const <BottomNavigationBarItem>[
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.location_on),
-                    label: 'Buqui\'s Home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.call_rounded),
-                    label: 'Call us',
-                  ),
-                ],
-                currentIndex: _selectedIndex,
-                selectedItemColor: Colors.white54,
-                onTap: _onItemTapped,
-              ),
-            )
+            Container(
+              color: Colors.transparent,
+              height: 200.0,
+              alignment: Alignment.center,
+              child: _widgetOptions.elementAt(_selectedIndex),
+            ),
+            Image.network(
+              'https://static.wixstatic.com/media/3efee9_d3d16f0e618040af97b0ad01abcd03b3~mv2.png/v1/fill/w_1184,h_360,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/cervezas_pngs.png',
+              fit: BoxFit.cover,
+            ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        shape: const CircleBorder(),
+        onPressed: _goToHomeScreen,
+        elevation: 1,
+        backgroundColor: Colors.cyan,
+        child: const Icon(Icons.favorite, color: Colors.white70),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xFFF9881F),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on),
+            label: 'Buqui\'s Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.call_rounded),
+            label: 'Call us',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white54,
+        onTap: _onItemTapped,
       ),
     );
   }
 
   void _goToHomeScreen() {
-    router.go('/');
+    router.go('/about');
   }
 }
--- lib/src/features/menu/menu.dart
+++ lib/src/features/menu/menu.dart
@@ -23,111 +23,113 @@ class _MyMenuState extends State<MyMenu> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: const Color(0xFFF7F7FB),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 20, right: 21, left: 21),
-          child: Stack(
-            children: [
-              Scaffold(
-                appBar: AppBar(
-                  flexibleSpace: const ContainerTheme(),
-                  backgroundColor: Colors.white,
-                  foregroundColor: Colors.white60,
-                  title: const Text('The Buqui Bichi Menu'),
-                  titleSpacing: 1.2,
-                  titleTextStyle: const TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      overflow: TextOverflow.fade,
-                      color: Colors.white60),
-                ),
-                body: PageView.builder(
-                  itemCount: foods.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Center(
-                      child: SizedBox(
-                        height: MediaQuery.of(context).size.height,
-                        width: MediaQuery.of(context).size.width,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Expanded(
-                              child: Image.network(
-                                (foods[index].foodImage),
-                                fit: BoxFit.cover,
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: const Color(0xFFF7F7FB),
+          body: Padding(
+            padding: const EdgeInsets.only(top: 20, right: 21, left: 21),
+            child: Stack(
+              children: [
+                Scaffold(
+                  appBar: AppBar(
+                    flexibleSpace: const ContainerTheme(),
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.white60,
+                    title: const Text('The Buqui Bichi Menu'),
+                    titleSpacing: 1.2,
+                    titleTextStyle: const TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        overflow: TextOverflow.fade,
+                        color: Colors.white60),
+                  ),
+                  body: PageView.builder(
+                    itemCount: foods.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Center(
+                        child: SizedBox(
+                          height: MediaQuery.of(context).size.height,
+                          width: MediaQuery.of(context).size.width,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: Image.network(
+                                  (foods[index].foodImage),
+                                  fit: BoxFit.cover,
+                                ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Column(
-                                children: [
-                                  Text(
-                                    foods[index].foodName,
-                                    style: const TextStyle(
-                                        fontSize: 27.0,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  const SizedBox(height: 12),
-                                  Text(
-                                    foods[index].burgerType,
-                                    style: const TextStyle(
-                                      fontSize: 19.0,
-                                      color: Colors.black38,
-                                      fontFamily: 'Spot mono',
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      foods[index].foodName,
+                                      style: const TextStyle(
+                                          fontSize: 27.0,
+                                          fontWeight: FontWeight.bold),
                                     ),
-                                  ),
-                                  const SizedBox(height: 12),
-                                  Text(
-                                    textAlign: TextAlign.center,
-                                    foods[index].foodInfo,
-                                    style: const TextStyle(
-                                      fontSize: 17.0,
-                                      color: Colors.black38,
-                                      fontFamily: 'Futura',
-                                      letterSpacing: 2,
-                                      wordSpacing: 5,
+                                    const SizedBox(height: 12),
+                                    Text(
+                                      foods[index].burgerType,
+                                      style: const TextStyle(
+                                        fontSize: 19.0,
+                                        color: Colors.black38,
+                                        fontFamily: 'Spot mono',
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Container(
-                              width: 200,
-                              height: 46,
-                              decoration: const BoxDecoration(
-                                color: Colors.transparent,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(55)),
-                              ),
-                              child: const Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Icon(Icons.arrow_left, color: Colors.cyan),
-                                  Text(
-                                    'Swipe',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.brown,
-                                      fontFamily: 'Notewhorty',
+                                    const SizedBox(height: 12),
+                                    Text(
+                                      textAlign: TextAlign.center,
+                                      foods[index].foodInfo,
+                                      style: const TextStyle(
+                                        fontSize: 17.0,
+                                        color: Colors.black38,
+                                        fontFamily: 'Futura',
+                                        letterSpacing: 2,
+                                        wordSpacing: 5,
+                                      ),
                                     ),
-                                  ),
-                                  Icon(Icons.arrow_right, color: Colors.cyan),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                            const MyItems(),
-                          ],
+                              Container(
+                                width: 200,
+                                height: 46,
+                                decoration: const BoxDecoration(
+                                  color: Colors.transparent,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(55)),
+                                ),
+                                child: const Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Icon(Icons.arrow_left, color: Colors.cyan),
+                                    Text(
+                                      'Swipe',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.brown,
+                                        fontFamily: 'Notewhorty',
+                                      ),
+                                    ),
+                                    Icon(Icons.arrow_right, color: Colors.cyan),
+                                  ],
+                                ),
+                              ),
+                              const MyItems(),
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 ),
-              ),
-            ],
-          ),
-        )
+              ],
+            ),
+          )
+      ),
     );
   }
 }
