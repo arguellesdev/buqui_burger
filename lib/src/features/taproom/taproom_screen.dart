b18d86c11e9a4a6e01af1f1cc65673f1feaddc83
Anya <arguellesdev@gmail.com>
2024-02-28 09:53:53 -0700

    transition to taproom

--- lib/src/features/taproom/taproom_screen.dart
+++ lib/src/features/taproom/taproom_screen.dart
@@ -1,8 +1,32 @@
+import 'dart:async';
+
 import 'package:buqui_burgers/src/common_widgets/container_theme.dart';
 import 'package:flutter/material.dart';
 
-class MyTapRoomScreen extends StatelessWidget {
+class MyTapRoomScreen extends StatefulWidget {
   const MyTapRoomScreen({super.key});
+
+  @override
+  State<MyTapRoomScreen> createState() => _MyTapRoomScreenState();
+}
+
+class _MyTapRoomScreenState extends State<MyTapRoomScreen> {
+  int _imageIndex = 0;
+  final List<String> _animatedImageUrls = [
+    'https://lumari.mx/wp-content/uploads/2019/07/BBN_08.jpg',
+    'https://lumari.mx/wp-content/uploads/2019/07/BBN_14.jpg',
+    'https://gourmetdemexico.com.mx/wp-content/uploads/2018/04/cerveceri%CC%81as-sonora.jpg',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(seconds: 3), (Timer timer) {
+      setState(() {
+        _imageIndex = (_imageIndex + 1) % _animatedImageUrls.length;
+      });
+    });
+  } //this time is for holding the image Anya
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +37,24 @@ class MyTapRoomScreen extends StatelessWidget {
           flexibleSpace: const ContainerTheme(),
           title: const Text('Tap Rooms'),
         ),
-        body: Column(
-          children: [
-            Image.network(
-              'https://lumari.mx/wp-content/uploads/2019/07/BBN_08.jpg',
-              fit: BoxFit.cover,
-            ),
-            const Text('Under construction'),
-          ],
+        body: Center(
+          child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 541), //this time is for images transition between Anya
+            reverseDuration: const Duration(milliseconds: 541),//this time is for cycle Anya
+            switchInCurve: Curves.easeInOut,
+            switchOutCurve: Curves.easeInOut,
+            key: ValueKey<int>(_imageIndex),
+            child: Image.network(_animatedImageUrls[_imageIndex]),
+            transitionBuilder: (Widget child, Animation<double> animation) {
+              return FadeTransition(
+                opacity: animation,
+                child: ScaleTransition(
+                  scale: animation,
+                  child: child,
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
